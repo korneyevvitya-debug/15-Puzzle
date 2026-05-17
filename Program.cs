6ffab392206f92e6a5a7f2dcee678cc4467d8924
timurmakhmudov <m4khmudov.t@yandex.ru>
2026-05-17 18:14:53 +0300

    Creating Menu 4 Game

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace chisla
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainMenuForm());
         }
     }
 }
